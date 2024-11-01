01f07099cb02878cafd4e1dfe31d76ee205eca44
Graham <graham.gibbons@gmail.com>
2024-11-01 14:40:14 -0700

    Edge length calculation

--- main.ts
+++ main.ts
@@ -3,12 +3,40 @@ const positions = [
   [0, 1],
   [1, 0],
   [1, 1],
-] as const;
+] satisfies [number, number][];
 
 const triangles = [
   [0, 1, 2],
   [1, 3, 2],
-] as const;
+] satisfies [number, number, number][];
+
+const edges = Object.values(
+  triangles.reduce<{
+    [key: string]: [a: number, b: number];
+  }>(
+    (acc, [a, b, c]) =>
+      [
+        [a, b],
+        [b, c],
+        [c, a],
+      ]
+        .map((_) => _.sort())
+        .reduce((acc, [a = 0, b = 0]) => {
+          acc[`${a},${b}`] = [a, b];
+          return acc;
+        }, acc),
+    {}
+  )
+);
+
+const sub = ([x1, y1]: [number, number], [x2, y2]: [number, number]) =>
+  [x1 - x2, y1 - y2] satisfies [number, number];
+
+const norm = ([x, y]: [number, number]) => Math.sqrt(x * x + y * y);
+
+const edgeLengths = edges.map(([a, b]) =>
+  norm(sub(positions[a] ?? [0, 0], positions[b] ?? [0, 0]))
+);
 
 async function init() {
   const { gpu } = navigator;
